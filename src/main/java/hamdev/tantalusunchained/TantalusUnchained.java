05984df847e43da349f60e7bc8e26f4d95686f49
Steve <trakaa@gmail.com>
2019-03-16 18:11:42 -0400

    TantalusUnchained.java initial commit

---
+++ src/main/java/hamdev/tantalusunchained/TantalusUnchained.java
@@ -0,0 +1,16 @@
+package hamdev.tantalusunchained;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+
+@Mod(modid = TantalusUnchained.MODID, name = TantalusUnchained.NAME, version = TantalusUnchained.VERSION)
+public class TantalusUnchained
+{
+    public static final String MODID = "tantalusunchained";
+    public static final String NAME = "Tantalus Unchained";
+    public static final String VERSION = "1.0.0.0";
+
+    private static Logger logger;
+
+    //Do some code, mofux!
+}
