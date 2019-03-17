50b636d700591aa09bae9a8fd43240306c7eceb4
Trakaa <trakaa@gmail.com>
2019-03-16 20:38:20 -0400

    Fixed deps with Forge.

--- src/main/java/hamdev/tantalusunchained/TantalusUnchained.java
+++ src/main/java/hamdev/tantalusunchained/TantalusUnchained.java
@@ -1,16 +1,25 @@
 package hamdev.tantalusunchained;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = TantalusUnchained.MODID, name = TantalusUnchained.NAME, version = TantalusUnchained.VERSION)
+import java.util.stream.Collectors;
+
+@Mod("tantalusunchained")
 public class TantalusUnchained
 {
-    public static final String MODID = "tantalusunchained";
-    public static final String NAME = "Tantalus Unchained";
-    public static final String VERSION = "1.0.0.0";
-
-    private static Logger logger;
-
     //Do some code, mofux!
 }
