d280a6659ab80c558ad9116c77814349c94e49ac
Trakaa <trakaa@gmail.com>
2019-03-18 21:18:13 -0400

    Added description to item.

--- src/main/java/hamdev/tantalusunchained/items/ItemHeavyMetal.java
+++ src/main/java/hamdev/tantalusunchained/items/ItemHeavyMetal.java
@@ -1,8 +1,15 @@
 package hamdev.tantalusunchained.items;
 
 import hamdev.tantalusunchained.TantalusUnchained;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+import java.util.List;
 
 public class ItemHeavyMetal extends Item
 {
@@ -10,5 +17,11 @@ public class ItemHeavyMetal extends Item
     {
         super(new Properties().group(TantalusUnchained.creativeTab));
         setRegistryName(new ResourceLocation(TantalusUnchained.MODID, "heavy_metal"));
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World player, List<ITextComponent> list, ITooltipFlag flag) {
+        super.addInformation(stack, player, list, flag);
+        list.add(new TextComponentString("Brent is a slut!"));
     }
 }
