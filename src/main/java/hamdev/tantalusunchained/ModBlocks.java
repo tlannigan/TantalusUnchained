423a409c7bc256ed13f26dafb2eb4654fa3dd62d
Trakaa <trakaa@gmail.com>
2019-03-23 14:44:36 -0400

    Added addInformation and itemblock test class.

--- src/main/java/hamdev/tantalusunchained/ModBlocks.java
+++ src/main/java/hamdev/tantalusunchained/ModBlocks.java
@@ -1,6 +1,7 @@
 package hamdev.tantalusunchained;
 
 import hamdev.tantalusunchained.blocks.BlockTest;
+import hamdev.tantalusunchained.items.itemblocks.ItemBlockTest;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -8,12 +9,13 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModBlocks {
     public static final Block blockTest = new BlockTest();
+    public static final ItemBlock itemBlockTest = new ItemBlockTest(blockTest, new Item.Properties().group(TantalusUnchained.creativeTab));
 
     public static void registerBlocks(IForgeRegistry<Block> registry) {
         registry.register(blockTest);
     }
 
     public static void registerItems(IForgeRegistry<Item> registry) {
-        registry.register(new ItemBlock(blockTest, new Item.Properties().group(TantalusUnchained.creativeTab)).setRegistryName(TantalusUnchained.MODID, "block_test"));
+        registry.register(itemBlockTest);
     }
 }
---
+++ src/main/java/hamdev/tantalusunchained/items/itemblocks/ItemBlockTest.java
@@ -0,0 +1,29 @@
+package hamdev.tantalusunchained.items.itemblocks;
+
+import hamdev.tantalusunchained.TantalusUnchained;
+import net.minecraft.block.Block;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class ItemBlockTest extends ItemBlock {
+    public ItemBlockTest(Block block, Item.Properties properties) {
+        super(block, properties);
+        setRegistryName(TantalusUnchained.MODID, "block_test");
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World player, List<ITextComponent> list, ITooltipFlag flag)
+    {
+        super.addInformation(stack, player, list, flag);
+
+        list.add(new TextComponentString(""));
+        list.add(new TextComponentString("\u00A79This is a test, bitch!"));
+    }
+}
