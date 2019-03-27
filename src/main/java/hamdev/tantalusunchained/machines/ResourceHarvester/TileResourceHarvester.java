7766a8e9e5c4804b98ba7822a349190c6ecdcbeb
Trakaa <trakaa@gmail.com>
2019-03-26 21:15:58 -0400

    Add tick counter and started NBT in/out.

--- src/main/java/hamdev/tantalusunchained/machines/ResourceHarvester/TileResourceHarvester.java
+++ src/main/java/hamdev/tantalusunchained/machines/ResourceHarvester/TileResourceHarvester.java
@@ -1,6 +1,8 @@
 package hamdev.tantalusunchained.machines.ResourceHarvester;
 
 import hamdev.tantalusunchained.ModBlocks;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import org.apache.logging.log4j.LogManager;
@@ -15,12 +17,35 @@ public class TileResourceHarvester extends TileEntity implements ITickable
         super(ModBlocks.TYPE_TEST);
     }
 
+    private Integer tickIterator;
+
     @Override
     public void tick()
     {
         if(!world.isRemote)
         {
+            if (tickIterator >= 6000) {
+                tickIterator = 0;
+                outputResource();
+            }
+            tickIterator ++;
             LOGGER.info("Ticking away");
         }
     }
+
+    private void outputResource() {
+        //Logic for NBT shit
+
+    }
+
+    //@Override
+    //public void deserializeNBT(NBTTagCompound nbt) {
+
+    //}
+
+    //@Override
+    //public NBTTagCompound serializeNBT() {
+
+        //  return ;
+    //}
 }
